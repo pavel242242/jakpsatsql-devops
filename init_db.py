64492c7906fb5531297b93d6889c98ff6747cb6d
pavel242242 <pavel@chocholous.net>
2020-02-18 12:47:59 +0000

    formatovani a komentare

--- init_db.py
+++ init_db.py
@@ -38,27 +38,31 @@ nodes = ("czechita", "uk", "pokrocili")
 #jeden kouc ;)
 print ("create role role_superkouc; \n" \
        "grant role role_superkouc to user chocholousp; \n"
+       "create role role_kouc; \n" \
        "/ * * * * * * * *  * * * * **********************************/ \n"
       )
 #a dalsi kouci
+print ("--vytvoreni koucu")
 for c in cg["coaches"]:
     u = c["name"]
     e = c["email"]
-    print (" \n" \
-    "create user "+u+" with_password = 'SpanekSeNeprecenuje', email = "+e+", must_change_password; \n")
+    print("create OR REPLACE user "+u+" password = 'SpanekSeNeprecenuje', email = '"+e+"', must_change_password = true, DEFAULT_WAREHOUSE = 'EXT_PROJECT', DEFAULT_NAMESPACE = 'CHOCHOLOUSP.PUBLIC', DEFAULT_ROLE = 'role_kouc';")
+    #print ("create user "+u+" with_password = 'SpanekSeNeprecenuje', email = "+e+", must_change_password; \n")
 #create user ondra password = 'SpanekSeNeprecenuje', email = 'hosak@avast.com', must_change_password = true, DEFAULT_WAREHOUSE = 'EXT_PROJECT', DEFAULT_NAMESPACE = 'CHOCHOLOUSP.PUBLIC', DEFAULT_ROLE = 'EXT_ADMIN';
 
 #vytvoreni schemat a roli pro kazdy node
+print ("--user login, schema, role")
 for node in cg["nodes"]:
     n = node["name"]
     print ("" \
-          "-- Vytvoreni schemat a roli node "+n+"; \n"
+          "-- Vytvoreni schemat a roli node "+n+"; \n"          
           "create schema "+n+"; \n"
           "create role role_"+n+"; \n" \
           "create role role_"+n+"_kouc; \n" \
           "create schema sch_"+n+"_hriste; \n"
           "create schema "+n+"_hriste; \n")
     lLogins = node["logins"]
+    print ("--kouc login")
     for u in lLogins:
         print (" \n" \
   "create user "+n+"_"+u+" with_password = 'SpanekSePrecenuje', defaultnamespace, computenode, etc ...; \n" \
@@ -81,18 +85,18 @@ for node in cg["nodes"]:
         "--grant role_"+n+"_kouc to "+u+"")
 print("uncomment at will */")
 
+print ("--grant select on schemas to roles")
 for node in cg["nodes"]:
     n = node["name"]
     print ("grant select on all tables in schema sch_"+n+" to role_"+n+"\n" \
-    "grant all on schema sch_"+n+" to role_"+n+"_kouc; \n")
+    "grant all on schema sch_"+n+" to role_"+n+"_kouc; ")
     for u in node["logins"]:
-        print (" \n" \
-        "grant role_"+n+" to role_"+n+"_"+u+"\n")
+        print ("grant role_"+n+" to role_"+n+"_"+u+"\n")
 
 for node in cg["nodes"]:
     n = node["name"]
     for u in node["logins"]:
-        print ("grant all on schema sch_"+n+"_"+u+" to role_"+n+"_"+u+";\n" \
+        print ("grant all on schema sch_"+n+"_"+u+" to role_"+n+"_"+u+";" \
          )
 #create schema $schema
 # role
