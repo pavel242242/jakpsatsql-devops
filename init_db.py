f32e2afec3a32404ff33a7b86a80f4abc3794dda
pavel242242 <pavel@chocholous.net>
2020-04-19 17:01:01 +0000

    roles for coaches, db access, warehouse, default role for kouc etc

--- init_db.py
+++ init_db.py
@@ -26,13 +26,32 @@ cg={ \
       },
       {"NAME"  : "BUBLINA",
       "EMAIL" : "BUBLINA@SEZNAM.COM"
+      },
+      {"NAME"  : "Monika",
+      "EMAIL" : "Monika@Monika.cz"
+      },
+      {"NAME"  : "Eva",
+      "EMAIL" : "Eva@Eva.cz"
+      },
+      {"NAME"  : "Julia",
+      "EMAIL" : "Julia@Julia.cz"
+      },
+      {"NAME"  : "Alena",
+      "EMAIL" : "Alena@Alena.cz"
+      },
+      {"NAME"  : "Katka",
+      "EMAIL" : "Katka@Katka.cz"
+      },
+      {"NAME"  : "Anna",
+      "EMAIL" : "Anna@anna.cz"
       }]
     }
 
 #jeden kouc ;)
 print ("CREATE OR REPLACE ROLE ROLE_SUPERKOUC; \n" \
-       "GRANT ROLE ROLE_SUPERKOUC TO USER CHOCHOLOUSP; \n"
+       "GRANT ROLE ROLE_SUPERKOUC TO USER sfadmin; \n"
        "CREATE OR REPLACE ROLE ROLE_KOUC; \n" \
+       "GRANT USAGE ON DATABASE COURSES TO ROLE ROLE_KOUC; \n" \
        "/* * * * * * * *  * * * * **********************************/ \n"
       )
 #a dalsi kouci
@@ -40,7 +59,8 @@ print ("--VYTVORENI KOUCU")
 for c in cg["COACHES"]:
     u = c["NAME"]
     e = c["EMAIL"]
-    print("CREATE OR REPLACE USER KOUC_"+u+" PASSWORD = 'SpANEKSeNePRECENUJE', EMAIL = '"+e+"', MUST_CHANGE_PASSWORD = TRUE, DEFAULT_WAREHOUSE = 'EXT_PROJECT', DEFAULT_NAMESPACE = '" + dbname +".PUBLIC', DEFAULT_ROLE = 'ROLE_KOUC';")
+    print("CREATE OR REPLACE USER KOUC_"+u+" PASSWORD = 'SpANEKSeNePRECENUJE', EMAIL = '"+e+"', MUST_CHANGE_PASSWORD = TRUE, DEFAULT_WAREHOUSE = 'EXT_PROJECT', DEFAULT_NAMESPACE = '" + dbname +".PUBLIC', DEFAULT_ROLE = 'ROLE_KOUC'; \n")
+    print("GRANT ROLE ROLE_KOUC TO USER KOUC_"+u+";")
 print("")
 #create user ondra password = 'SPANEKSENEPRECENUJE', email = 'HOSAK@AVAST.COM', must_change_password = true, DEFAULT_WAREHOUSE = 'EXT_PROJECT', DEFAULT_NAMESPACE = 'CHOCHOLOUSP.PUBLIC', DEFAULT_ROLE = 'EXT_ADMIN';
 
@@ -50,15 +70,15 @@ for node in cg["NODES"]:
     n = node["NAME"]
     print ("" \
           "-- VYTVORENI SCHEMAT A ROLI NODE "+n+"; \n"          
-          "CREATE ROLE ROLE_"+n+"; \n" \
-          "CREATE ROLE ROLE_"+n+"_KOUC; \n" \
-          "CREATE SCHEMA SCH_"+n+"; \n"
-          "CREATE SCHEMA SCH_"+n+"_HRISTE; \n")
+          "CREATE OR REPLACE ROLE ROLE_"+n+"; \n" \
+          "CREATE OR REPLACE ROLE ROLE_"+n+"_KOUC; \n" \
+          "CREATE OR REPLACE SCHEMA SCH_"+n+"; \n"
+          "CREATE OR REPLACE SCHEMA SCH_"+n+"_HRISTE; \n")
     lLogins = node["LOGINS"]
     for u in lLogins:
         print (" \n" \
-        "CREATE ROLE ROLE_"+n+"_"+u+";\n" \
-        "CREATE SCHEMA SCH_"+n+"_"+u+";\n" \
+        "CREATE OR REPLACE ROLE ROLE_"+n+"_"+u+";\n" \
+        "CREATE OR REPLACE SCHEMA SCH_"+n+"_"+u+";\n" \
         "CREATE OR REPLACE USER "+n+"_"+u+" PASSWORD = 'SpANEKSePrECENUJE', MUST_CHANGE_PASSWORD = TRUE, DEFAULT_WAREHOUSE = 'EXT_PROJECT', DEFAULT_NAMESPACE = '" + dbname + "." +n+"', DEFAULT_ROLE = 'ROLE_"+n+"'; \n" \
         )
 
@@ -75,14 +95,14 @@ for node in cg["NODES"]:
     for c in cg["COACHES"]:
         u = c["NAME"]
         print ("" \
-        "--GRANT ROLE_"+n+"_KOUC TO ROLE KOUC_"+u+"")
+        "GRANT ROLE ROLE_"+n+"_KOUC TO USER KOUC_"+u+";")
 print("UNCOMMENT AT WILL */ \n")
 
 print ("--GRANT SELECT ON SCHEMAS TO ROLES")
 for node in cg["NODES"]:
     n = node["NAME"]
     print ("GRANT USAGE ON DATABASE COURSES TO ROLE ROLE_"+n+"; \n" \
-    "GRANT USAGE ON WAREHOUSE EXT_PROJECT01 TO ROLE ROLE_"+n+"; \n" \
+    "GRANT USAGE ON WAREHOUSE EXT_PROJECT TO ROLE ROLE_"+n+"; \n" \
     "GRANT USAGE ON SCHEMA SCH_"+n+" TO ROLE ROLE_"+n+"; \n" \
     "GRANT SELECT ON ALL TABLES IN SCHEMA SCH_"+n+" TO ROLE ROLE_"+n+"; \n" \
     "GRANT SELECT ON FUTURE TABLES IN SCHEMA SCH_"+n+" TO ROLE ROLE_"+n+"; \n" \
