dc8a4214088ba52faf5a6b673e821d6af4df6d15
Pavel Chocholouš <pavel@chocholous.net>
2022-08-29 09:26:17 +0000

    dapj22 users fix teror files for mac

--- init_db.py
+++ init_db.py
@@ -1,108 +1,184 @@
+f = open("logins.sql", "w")
+f.write("--this is generated login file \n")
+
+
+
 dbname = "COURSES"
 cg={ \
   "NODES": [
+
     {"NAME" : "CZECHITA",
-     "LOGINS" : ["balazoval",	"barkovaj",	"bartosovak",	"beranovam",	"bernardovaa",	"camprovan",	"coblovas",	"cervenkovai",	"cizkovae",	"hrubaj",	"hubinkovab",	"chytilovah",	"knirovat",	"konecnav",	"korycanovaj",	"kruckad",	"merickovas",	"michlovam",	"moravkovap",	"nadaskap",	"novad",	"otcenasovam",	"polednovap",	"sommerovag",	"strokovaa",	"sukovam",	"synkovak",	"teglasiovav",	"torokovas",	"vectenn",	"zabranskad",	"zavadiloval",]
-    },
-    {"NAME" : "UK",
-     "LOGINS" : ["adamect",	"boxanovap",	"brabecm",	"danovskyr",	"fidranskyt",	"iarulinz",	"janovskyj",	"krizovak",	"linhartovaa",	"malkusd",	"novotnat",	"otradovcovab",	"paucinovaa",	"perelygink",	"piekarzo",	"rebikovam",	"skurcakovak",	"stehnovaa",	"strykav",	"stetkaj",	"svejkovat",	"zelenkaf",	"zemlickovat",	"dostalm",	"duseka",	"epikhinav",	"karbanovak",	"kleckovad",	"kodesj",	"kovarikovae",	"krejcovas",	"ligro",	"linerj",	"melicharf",	"navratilovat",	"opltovam",	"petrzelkovat",	"samecp",	"sedajm",	"smidd",	"storovaa",	"sustal",]
-    },
-    {"NAME" : "POKROCILI",
-     "LOGINS" : ["01", "02"]
+     "LOGINS" : [   {"username": "TEST", "email": "pavel@chocholous.net"},
+{"username": "BAJEROVAK", "email": "kacaba.bajerova@gmail.com"},
+{"username": "BALESNAT", "email": "tbalesna@gmail.com"},
+{"username": "BECKOVAA", "email": "adela.beckova@gmail.com"},
+{"username": "BERANOVAH", "email": "hanina.kohoutova@gmail.com"},
+{"username": "CESAROVAL", "email": "cesarova@gmail.com"},
+{"username": "DVORAKOVAV", "email": "vratenka@gmail.com"},
+{"username": "ESNEROVAE", "email": "eva.esnerova@gmail.com "},
+{"username": "FRANKOVAI", "email": "irmulkairmulka@gmail.com"},
+{"username": "HLAVNICKOVAD", "email": "dominika.hlavnickova@gmail.com"},
+{"username": "HRACHOVCOVAL", "email": "lenka.hrachovcova@gmail.com"},
+{"username": "HRISTOVAK", "email": "kl.hristova15@gmail.com"},
+{"username": "JELINKOVAZ", "email": "jelinzor@gmail.com"},
+{"username": "JONASOVAM", "email": "farida.martina@gmail.com"},
+{"username": "KRCHOVAI", "email": "iva.krchova@gmail.com"},
+{"username": "KRSNAKOVAK", "email": "katka.krsnak@gmail.com"},
+{"username": "KUTHANOVAH", "email": "helena.kuthanova@gmail.com"},
+{"username": "KVARDOVAK", "email": "kvardova.karolina@gmail.com"},
+{"username": "MIKUSKOVAM", "email": "mmikuskova@gmail.com"},
+{"username": "MLADKOVAP", "email": "petra.mladek@gmail.com"},
+{"username": "NECKAROVAZ", "email": "rodinaneckarova@gmail.com"},
+{"username": "PROKUPKOVAR", "email": "prokupkovar@gmail.com"},
+{"username": "REHAKOVAM", "email": "magda.husakova@gmail.com"},
+{"username": "SEHNALOVAB", "email": "sehnalova.barca@gmail.com"},
+{"username": "SEMEROVAM", "email": "martinamatasovska2@gmail.com"},
+{"username": "SVOBODOVAM", "email": "horcice@gmail.com"},
+{"username": "SVOBODOVAO", "email": "olga.svobo@gmail.com"},
+{"username": "SALENAL", "email": "lucie.salena@gmail.com"},
+{"username": "TARANCHUKV", "email": "vt.vinco@gmail.com"},
+{"username": "TATARNIKOVAI", "email": "juliya.tatarnikova@gmail.com"},
+{"username": "TATAUSHE", "email": "tataushelena@gmail.com"},
+{"username": "TUROVAZ", "email": "turovazanna0@gmail.com"},
+{"username": "VAJSOVAT", "email": "vajsova.tatiana@gmail.com"},
+{"username": "VASKUK", "email": "katerina.vasku@gmail.com"},
+{"username": "VAVROVAV", "email": "veronika.bednarikova@gmail.com"},
+{"username": "ABSOLONOVAM", "email": "michaelka.absolonova@gmail.com"},
+{"username": "AMBROZOVAJ", "email": "janiambrozova@gmail.com"},
+{"username": "CAIDLEROVAS", "email": "caidlerova@gmail.com"},
+{"username": "ERMAKOVAN", "email": "nadezda.epifanova@gmail.com"},
+{"username": "FIALOVAM", "email": "michaela.lunakova@gmail.com"},
+{"username": "FISARKOVAJ", "email": "fisarkovaj@gmail.com"},
+{"username": "HANKOVAL", "email": "lenkaa.hankova@gmail.com"},
+{"username": "JAKABOVAL", "email": "ludmila.jakabova01@gmail.com"},
+{"username": "JIRKOVAA", "email": "anetajirkova@gmail.com"},
+{"username": "KADLECOVAJ", "email": "jana.kadlecova@gmail.com"},
+{"username": "KARASZOVAL", "email": "lenka.karaszov@gmail.com"},
+{"username": "KOLAROVAH", "email": "kolarovahelena777@gmail.com"},
+{"username": "KONECNAH", "email": "hanka.patio@GMAIL.COM"},
+{"username": "MONIOVAE", "email": "eva.moniova@gmail.com"},
+{"username": "PECHOVAE", "email": "eveetka@gmail.com"},
+{"username": "POKORNAS", "email": "anomis.p@gmail.com"},
+{"username": "POSTOVAH", "email": "Postova.helena@gmail.com"},
+{"username": "PRIHODOVAA", "email": "alena.prihodova@gmail.com"},
+{"username": "SNITILOVAG", "email": "snitilovag@gmail.com"},
+{"username": "SRBOVAE", "email": "eva.srbova.g@gmail.com"},
+{"username": "STEJSKALOVAK", "email": "karolina.preislerova@gmail.com"},
+{"username": "SVOBODOVAH", "email": "budska.helena@gmail.com"},
+{"username": "SIMERDOVA SVEDOVAK", "email": "katerina.svedova@gmail.com"},
+{"username": "SKVOROVAK", "email": "kkoutn@gmail.com"},
+{"username": "SLAPAKOVAM", "email": "Mia.Slapakova@gmail.com"},
+{"username": "STEFANKOVAK", "email": "stefankova.kristyna@gmail.com"},
+{"username": "STEPANIKOVAS", "email": "sarka.stepanikova81@gmail.com"},
+{"username": "STRAJTOVAM", "email": "strajtova.mariana@gmail.com"},
+{"username": "TABORSKAM", "email": "monista.f@gmail.com"},
+{"username": "USTENKOO", "email": "am.ustenko@gmail.com"},
+{"username": "VALKOVSKAA", "email": "adriana.valkovska@gmail.com"},
+{"username": "VEDRALOVAA", "email": "avlasicova39@gmail.com"},
+{"username": "VOYNALOVYCHV", "email": "vojnalovicsv@gmail.com"},
+{"username": "ZHYZHDAL", "email": "zizdaluda@gmail.com"},
+{"username": "NAHRADNIK01_DPP22", "email": "tereza.szkatulova@czechitas.cz"},
+{"username": "NAHRADNIK02_DPP22", "email": "tereza.szkatulova@czechitas.cz"},
+{"username": "NAHRADNIK03_DPP22", "email": "tereza.szkatulova@czechitas.cz"},
+{"username": "NAHRADNIK04_DPP22", "email": "tereza.szkatulova@czechitas.cz"},
+{"username": "NAHRADNIK05_DPP22", "email": "tereza.szkatulova@czechitas.cz"},
+                ]
     }
 ],
 "COACHES": [
-      {"NAME"  : "ONDRA",
-      "EMAIL" : "HOSAK@AVAST.COM"
-      },
-      {"NAME"  :"VERONIKA",
-      "EMAIL" : "VERONIKA@GMAIL.COM"
-      },
-      {"NAME"  : "PAVLA",
-      "EMAIL" : "JIRKOVA.PA@GMAIL.COM"
-      },
-      {"NAME"  : "OLGA",
-      "EMAIL" : "OLGA.KURTINOVA@NATUR.CUNI.CZ"
-      },
-      {"NAME"  : "BUBLINA",
-      "EMAIL" : "BUBLINA@SEZNAM.COM"
-      },
-      {"NAME"  : "Monika",
-      "EMAIL" : "Monika@Monika.cz"
-      },
-      {"NAME"  : "Eva",
-      "EMAIL" : "Eva@Eva.cz"
-      },
-      {"NAME"  : "Julia",
-      "EMAIL" : "Julia@Julia.cz"
-      },
-      {"NAME"  : "Alena",
-      "EMAIL" : "Alena@Alena.cz"
-      },
-      {"NAME"  : "Katka",
-      "EMAIL" : "Katka@Katka.cz"
-      },
-      {"NAME"  : "Anna",
-      "EMAIL" : "Anna@anna.cz"
-      }]
+{"NAME": "LEKTOR01_PDD22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "LEKTOR02_PDD22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "LEKTOR03_PDD22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "LEKTOR04_PDD22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "LEKTOR05_PDD22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "LEKTOR06_PDD22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "LEKTOR07_PDD22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "LEKTOR08_PDD22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "LEKTOR09_PDD22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "LEKTOR10_PDD22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "", "EMAIL": ""},
+{"NAME": "KOUC01_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC02_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC03_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC04_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC05_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC06_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC07_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC08_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC09_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC10_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC11_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC12_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC13_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC14_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC15_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC16_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC17_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC18_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC19_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+{"NAME": "KOUC20_DPP22", "EMAIL": "tereza.szkatulova@czechitas.cz"},
+  
+]
     }
 
 #jeden kouc ;)
-print ("CREATE OR REPLACE ROLE ROLE_SUPERKOUC; \n" \
+f.write("CREATE OR REPLACE ROLE ROLE_SUPERKOUC; \n" \
        "GRANT ROLE ROLE_SUPERKOUC TO USER sfadmin; \n"
        "CREATE OR REPLACE ROLE ROLE_KOUC; \n" \
        "GRANT USAGE ON DATABASE COURSES TO ROLE ROLE_KOUC; \n" \
        "/* * * * * * * *  * * * * **********************************/ \n"
       )
 #a dalsi kouci
-print ("--VYTVORENI KOUCU")
+f.write("--VYTVORENI KOUCU")
 for c in cg["COACHES"]:
     u = c["NAME"]
     e = c["EMAIL"]
-    print("CREATE OR REPLACE USER KOUC_"+u+" PASSWORD = 'SpANEKSeNePRECENUJE', EMAIL = '"+e+"', MUST_CHANGE_PASSWORD = TRUE, DEFAULT_WAREHOUSE = 'EXT_PROJECT', DEFAULT_NAMESPACE = '" + dbname +".PUBLIC', DEFAULT_ROLE = 'ROLE_KOUC'; \n")
-    print("GRANT ROLE ROLE_KOUC TO USER KOUC_"+u+";")
-print("")
-#create user ondra password = 'SPANEKSENEPRECENUJE', email = 'HOSAK@AVAST.COM', must_change_password = true, DEFAULT_WAREHOUSE = 'EXT_PROJECT', DEFAULT_NAMESPACE = 'CHOCHOLOUSP.PUBLIC', DEFAULT_ROLE = 'EXT_ADMIN';
+    f.write("CREATE OR REPLACE USER KOUC_"+u+" PASSWORD = 'SpANEKSeNePRECENUJE', EMAIL = '"+e+"', MUST_CHANGE_PASSWORD = TRUE, DEFAULT_WAREHOUSE = 'COMPUTE_WH', DEFAULT_NAMESPACE = '" + dbname +".PUBLIC', DEFAULT_ROLE = 'ROLE_KOUC'; \n")
+    f.write("GRANT ROLE ROLE_KOUC TO USER KOUC_"+u+"; \n")
+f.write("\n")
+#create user ondra password = 'SPANEKSENEPRECENUJE', email = 'HOSAK@AVAST.COM', must_change_password = true, DEFAULT_WAREHOUSE = 'COMPUTE_WH', DEFAULT_NAMESPACE = 'CHOCHOLOUSP.PUBLIC', DEFAULT_ROLE = 'EXT_ADMIN';
 
 #vytvoreni schemat a roli pro kazdy node
-print ("--USER LOGIN, SCHEMA, ROLE")
+f.write("--USER LOGIN, SCHEMA, ROLE \n")
 for node in cg["NODES"]:
     n = node["NAME"]
-    print ("" \
-          "-- VYTVORENI SCHEMAT A ROLI NODE "+n+"; \n"          
+    f.write("-- VYTVORENI SCHEMAT A ROLI NODE "+n+"; \n"          
           "CREATE OR REPLACE ROLE ROLE_"+n+"; \n" \
           "CREATE OR REPLACE ROLE ROLE_"+n+"_KOUC; \n" \
           "CREATE OR REPLACE SCHEMA SCH_"+n+"; \n"
           "CREATE OR REPLACE SCHEMA SCH_"+n+"_HRISTE; \n")
+    #vytvoreni teroru
+    f.write("create or replace TABLE SCH_"+n+".TEROR_DPJ22 (   EVENTID NUMBER(38,0),   IYEAR NUMBER(38,0),   IMONTH NUMBER(38,0),   IDAY NUMBER(38,0),   APPROXDATE VARCHAR(16777216),   EXTENDED NUMBER(38,0),   RESOLUTION NUMBER(38,0),   COUNTRY NUMBER(38,0),   COUNTRY_TXT VARCHAR(16777216),   REGION NUMBER(38,0),   REGION_TXT VARCHAR(16777216),   PROVSTATE VARCHAR(16777216),   CITY VARCHAR(16777216),   LATITUDE NUMBER(12,8),   LONGITUDE NUMBER(12,8),   SPECIFICITY NUMBER(38,0),   VICINITY NUMBER(38,0),   LOCATION VARCHAR(16777216),   SUMMARY VARCHAR(16777216),   CRIT1 NUMBER(38,0),   CRIT2 NUMBER(38,0),   CRIT3 NUMBER(38,0),   DOUBTTERR NUMBER(38,0),   ALTERNATIVE NUMBER(38,0),   ALTERNATIVE_TXT VARCHAR(16777216),   MULTIPLE NUMBER(38,0),   SUCCESS NUMBER(38,0),   SUICIDE NUMBER(38,0),   ATTACKTYPE1 NUMBER(38,0),   ATTACKTYPE1_TXT VARCHAR(16777216),   ATTACKTYPE2 NUMBER(38,0),   ATTACKTYPE2_TXT VARCHAR(16777216),   ATTACKTYPE3 NUMBER(38,0),   ATTACKTYPE3_TXT VARCHAR(16777216),   TARGTYPE1 NUMBER(38,0),   TARGTYPE1_TXT VARCHAR(16777216),   TARGSUBTYPE1 NUMBER(38,0),   TARGSUBTYPE1_TXT VARCHAR(16777216),   CORP1 VARCHAR(16777216),   TARGET1 VARCHAR(16777216),   NATLTY1 NUMBER(38,0),   NATLTY1_TXT VARCHAR(16777216),   TARGTYPE2 NUMBER(38,0),   TARGTYPE2_TXT VARCHAR(16777216),   TARGSUBTYPE2 NUMBER(38,0),   TARGSUBTYPE2_TXT VARCHAR(16777216),   CORP2 VARCHAR(16777216),   TARGET2 VARCHAR(16777216),   NATLTY2 NUMBER(38,0),   NATLTY2_TXT VARCHAR(16777216),   TARGTYPE3 NUMBER(38,0),   TARGTYPE3_TXT VARCHAR(16777216),   TARGSUBTYPE3 NUMBER(38,0),   TARGSUBTYPE3_TXT VARCHAR(16777216),   CORP3 VARCHAR(16777216),   TARGET3 VARCHAR(16777216),   NATLTY3 NUMBER(38,0),   NATLTY3_TXT VARCHAR(16777216),   GNAME VARCHAR(16777216),   GSUBNAME VARCHAR(16777216),   GNAME2 VARCHAR(16777216),   GSUBNAME2 VARCHAR(16777216),   GNAME3 VARCHAR(16777216),   GSUBNAME3 VARCHAR(16777216),   MOTIVE VARCHAR(16777216),   GUNCERTAIN1 NUMBER(38,0),   GUNCERTAIN2 NUMBER(38,0),   GUNCERTAIN3 NUMBER(38,0),   INDIVIDUAL NUMBER(38,0),   NPERPS NUMBER(38,0),   NPERPCAP NUMBER(38,0),   CLAIMED NUMBER(38,0),   CLAIMMODE NUMBER(38,0),   CLAIMMODE_TXT VARCHAR(16777216),   CLAIM2 VARCHAR(16777216),   CLAIMMODE2 VARCHAR(16777216),   CLAIMMODE2_TXT VARCHAR(16777216),   CLAIM3 VARCHAR(16777216),   CLAIMMODE3 VARCHAR(16777216),   CLAIMMODE3_TXT VARCHAR(16777216),   COMPCLAIM VARCHAR(16777216),   WEAPTYPE1 NUMBER(38,0),   WEAPTYPE1_TXT VARCHAR(16777216),   WEAPSUBTYPE1 NUMBER(38,0),   WEAPSUBTYPE1_TXT VARCHAR(16777216),   WEAPTYPE2 NUMBER(38,0),   WEAPTYPE2_TXT VARCHAR(16777216),   WEAPSUBTYPE2 NUMBER(38,0),   WEAPSUBTYPE2_TXT VARCHAR(16777216),   WEAPTYPE3 NUMBER(38,0),   WEAPTYPE3_TXT VARCHAR(16777216),   WEAPSUBTYPE3 NUMBER(38,0),   WEAPSUBTYPE3_TXT VARCHAR(16777216),   WEAPTYPE4 NUMBER(38,0),   WEAPTYPE4_TXT VARCHAR(16777216),   WEAPSUBTYPE4 NUMBER(38,0),   WEAPSUBTYPE4_TXT VARCHAR(16777216),   WEAPDETAIL VARCHAR(16777216),   NKILL NUMBER(38,0),   NKILLUS NUMBER(38,0),   NKILLTER NUMBER(38,0),   NWOUND NUMBER(38,0),   NWOUNDUS NUMBER(38,0),   NWOUNDTE NUMBER(38,0),   PROPERTY NUMBER(38,0),   PROPEXTENT NUMBER(38,0),   PROPEXTENT_TXT VARCHAR(16777216),   PROPVALUE NUMBER(38,0),   PROPCOMMENT VARCHAR(16777216),   ISHOSTKID NUMBER(38,0),   NHOSTKID NUMBER(38,0),   NHOSTKIDUS NUMBER(38,0),   NHOURS NUMBER(38,0),   NDAYS NUMBER(38,0),   DIVERT VARCHAR(16777216),   KIDHIJCOUNTRY VARCHAR(16777216),   RANSOM NUMBER(38,0),   RANSOMAMT NUMBER(16,4),   RANSOMAMTUS NUMBER(16,4),   RANSOMPAID NUMBER(16,4),   RANSOMPAIDUS NUMBER(16,4),   RANSOMNOTE VARCHAR(16777216),   HOSTKIDOUTCOME NUMBER(38,0),   HOSTKIDOUTCOME_TXT VARCHAR(16777216),   NRELEASED NUMBER(38,0),   ADDNOTES VARCHAR(16777216),   SCITE1 VARCHAR(16777216),   SCITE2 VARCHAR(16777216),   SCITE3 VARCHAR(16777216),   DBSOURCE VARCHAR(16777216),   INT_LOG NUMBER(38,0),   INT_IDEO NUMBER(38,0),   INT_MISC NUMBER(38,0),   INT_ANY NUMBER(38,0),   RELATED VARCHAR(16777216)  ); \n")
     lLogins = node["LOGINS"]
-    for u in lLogins:
-        print (" \n" \
+    for cred in lLogins:
+        u = cred["username"]
+        e = cred["email"]
+        f.write(" \n" \
         "CREATE OR REPLACE ROLE ROLE_"+n+"_"+u+";\n" \
         "CREATE OR REPLACE SCHEMA SCH_"+n+"_"+u+";\n" \
-        "CREATE OR REPLACE USER "+n+"_"+u+" PASSWORD = 'SpANEKSePrECENUJE', MUST_CHANGE_PASSWORD = TRUE, DEFAULT_WAREHOUSE = 'EXT_PROJECT', DEFAULT_NAMESPACE = '" + dbname + "." +n+"', DEFAULT_ROLE = 'ROLE_"+n+"'; \n" \
+        "CREATE OR REPLACE USER "+n+"_"+u+" PASSWORD = 'SpANEKSePrECENUJE', EMAIL = '"+e+"', MUST_CHANGE_PASSWORD = TRUE, DEFAULT_WAREHOUSE = 'COMPUTE_WH', DEFAULT_NAMESPACE = '" + dbname + ".SCH_" +n+"', DEFAULT_ROLE = 'ROLE_"+n+"_"+u+"'; \n" \
         )
-
 #granty
 for node in cg["NODES"]:
     n = node["NAME"]
-    print ("-- GRANTNUTI PRAV KOUCUM PRO NODE "+n+"; \n"
+    f.write("-- GRANTNUTI PRAV KOUCUM PRO NODE "+n+"; \n"
            "GRANT ROLE ROLE_"+n+"_KOUC TO ROLE ROLE_SUPERKOUC; \n" \
      "GRANT ROLE ROLE_"+n+" TO ROLE ROLE_"+n+"_KOUC; \n" )
-print(" /*UNCOMMENT AT WILL")
+f.write(" /*UNCOMMENT AT WILL \n")
 for node in cg["NODES"]:
     n = node["NAME"]
-    print("-- ROLES FOR " + n)
+    f.write("-- ROLES FOR " + n + "\n")
     for c in cg["COACHES"]:
         u = c["NAME"]
-        print ("" \
-        "GRANT ROLE ROLE_"+n+"_KOUC TO USER KOUC_"+u+";")
-print("UNCOMMENT AT WILL */ \n")
+        f.write("" \
+        "GRANT ROLE ROLE_"+n+"_KOUC TO USER KOUC_"+u+"; \n")
+f.write("UNCOMMENT AT WILL */ \n")
 
-print ("--GRANT SELECT ON SCHEMAS TO ROLES")
+f.write("\n--GRANT SELECT ON SCHEMAS TO ROLES \n")
 for node in cg["NODES"]:
     n = node["NAME"]
-    print ("GRANT USAGE ON DATABASE COURSES TO ROLE ROLE_"+n+"; \n" \
-    "GRANT USAGE ON WAREHOUSE EXT_PROJECT TO ROLE ROLE_"+n+"; \n" \
+    f.write("GRANT USAGE ON DATABASE COURSES TO ROLE ROLE_"+n+"; \n" \
+    "GRANT USAGE ON WAREHOUSE COMPUTE_WH TO ROLE ROLE_"+n+"; \n" \
     "GRANT USAGE ON SCHEMA SCH_"+n+" TO ROLE ROLE_"+n+"; \n" \
     "GRANT SELECT ON ALL TABLES IN SCHEMA SCH_"+n+" TO ROLE ROLE_"+n+"; \n" \
     "GRANT SELECT ON FUTURE TABLES IN SCHEMA SCH_"+n+" TO ROLE ROLE_"+n+"; \n" \
@@ -111,23 +187,28 @@ for node in cg["NODES"]:
     "GRANT SELECT ON FUTURE TABLES IN SCHEMA SCH_"+n+"_HRISTE TO ROLE ROLE_"+n+"; \n" \
     "GRANT ALL ON SCHEMA SCH_"+n+" TO ROLE ROLE_"+n+"_KOUC; \n"
     "GRANT ALL ON ALL TABLES IN SCHEMA SCH_"+n+" TO ROLE ROLE_"+n+"_KOUC; \n"
-    "GRANT ALL ON FUTURE TABLES IN SCHEMA SCH_"+n+" TO ROLE ROLE_"+n+"_KOUC; ")
-    for u in node["LOGINS"]:
-        print ("GRANT ROLE ROLE_"+n+" TO ROLE ROLE_"+n+"_"+u+";")
+    "GRANT ALL ON FUTURE TABLES IN SCHEMA SCH_"+n+" TO ROLE ROLE_"+n+"_KOUC; \n \n")
+    for cred in node["LOGINS"]:
+        u = cred["username"]
+        f.write("GRANT ROLE ROLE_"+n+" TO ROLE ROLE_"+n+"_"+u+";\n")
 
 for node in cg["NODES"]:
     n = node["NAME"]
-    for u in node["LOGINS"]:
-        print ("GRANT ALL ON SCHEMA SCH_"+n+"_"+u+" TO ROLE ROLE_"+n+"_"+u+"; \n" \
+    for cred in node["LOGINS"]:
+        u = cred["username"]
+        f.write("GRANT ALL ON SCHEMA SCH_"+n+"_"+u+" TO ROLE ROLE_"+n+"_"+u+"; \n" \
         "GRANT ROLE ROLE_"+n+"_"+u+" TO USER "+n+"_"+u+"; \n" \
         "GRANT ALL ON ALL TABLES IN SCHEMA SCH_"+n+"_"+u+" TO ROLE ROLE_"+n+"_"+u+"; \n" \
         "GRANT ALL ON FUTURE TABLES IN SCHEMA SCH_"+n+"_"+u+" TO ROLE ROLE_"+n+"_"+u+"; \n" \
         "GRANT ALL ON SCHEMA SCH_"+n+"_"+u+" TO ROLE ROLE_"+n+"_KOUC; \n" \
         "GRANT ALL ON FUTURE TABLES IN SCHEMA SCH_"+n+"_"+u+" TO ROLE ROLE_"+n+"_KOUC; \n"
-        "GRANT ALL ON ALL TABLES IN SCHEMA SCH_"+n+"_"+u+" TO ROLE ROLE_"+n+"_KOUC;" )
+        "GRANT ALL ON ALL TABLES IN SCHEMA SCH_"+n+"_"+u+" TO ROLE ROLE_"+n+"_KOUC; \n" )
 #create schema $schema
 # role
 # ownership
 # hriste
 # testdata
 # clenstvi v role_czechita
+
+
+f.close()
